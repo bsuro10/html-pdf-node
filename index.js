47daba0c12391acea9c03225fd4a990a6de1e5f7
unknown <bardromi@gmail.com>
2023-08-17 12:17:51 +0300

    Remove inline-css package

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const puppeteer = require('puppeteer');
 var Promise = require('bluebird');
 const hb = require('handlebars')
-const inlineCss = require('inline-css')
 module.exports
 async function generatePdf(file, options, callback) {
   // we are using headless mode
@@ -20,8 +19,8 @@ async function generatePdf(file, options, callback) {
   const page = await browser.newPage();
 
   if(file.content) {
-    data = await inlineCss(file.content, {url:"/"});
-    console.log("Compiling the template with handlebars")
+    data = file.content;
+    console.log("Compiling the template with handlebars");
     // we have compile our code with handlebars
     const template = hb.compile(data, { strict: true });
     const result = template(data);
@@ -62,8 +61,8 @@ async function generatePdfs(files, options, callback) {
   const page = await browser.newPage();
   for(let file of files) {
     if(file.content) {
-      data = await inlineCss(file.content, {url:"/"})
-      console.log("Compiling the template with handlebars")
+      data = file.content;
+      console.log("Compiling the template with handlebars");
       // we have compile our code with handlebars
       const template = hb.compile(data, { strict: true });
       const result = template(data);
